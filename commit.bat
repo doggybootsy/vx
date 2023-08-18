d6f0f0227abf141ba7e0b415cd228561c0743f73
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-08-18 12:40:03 -0700

    test commit with .bat

--- commit.bat
+++ commit.bat
@@ -1,4 +1,14 @@
 @echo off
+for /F "tokens=2" %%i in ('date /t') do set date=%%i
+IF "%~1" == "" (set commitName="%date%:%time%")
+IF NOT "%~1" == "" (set commitName="%~1")
+IF "%~2" == "" (set branch="main")
+IF NOT "%~2" == "" (set branch="%~2")
+
+echo %commitName%
+echo %branch%
+
 git add .
-git commit -m %1
-echo %2
+git commit -m %commitName%
+git branch -m %branch%
+git puch -u origin %branch%
