f8552425c04ffa8c5f23f3fd70304737262f923a
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-08-23 20:45:43 -0700

    Test with description
    Testing with descriptions

--- commit.bat
+++ commit.bat
@@ -1,11 +1,20 @@
 @echo off
-for /F "tokens=2" %%i in ('date /t') do set date=%%i
-IF "%~1" == "" (set commitName="%date%:%time%")
-IF NOT "%~1" == "" (set commitName="%~1")
-IF "%~2" == "" (set branch="main")
-IF NOT "%~2" == "" (set branch="%~2")
+for /F "tokens=2" %%i in ('date /t') do SET date=%%i
+IF "%~1" == "" (SET commitName="%date%:%time%")
+IF NOT "%~1" == "" (SET commitName="%~1")
+IF "%~2" == "" (SET description="No Description Provided")
+IF NOT "%~2" == "" (SET description="%~2")
+IF "%~3" == "" (SET branch="main")
+IF NOT "%~3" == "" (SET branch="%~3")
 
 git add .
-git commit -m %commitName%
+git commit -m %commitName% -m %description%
 git branch -m %branch%
 git push -u origin %branch%
+
+for /f %%i in ('git rev-parse origin/%branch%') do (SET "hash=%%i")
+
+echo BRANCH: %branch%
+echo COMMIT NAME: %commitName%
+echo COMMIT DESCRIPTION: %description%
+echo https://github.com/doggybootsy/vx/commit/%hash%
