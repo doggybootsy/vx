e105c27312d8d6c1f85fe1e3cd81687eb79af139
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-08-28 03:31:14 -0700

    08/28/2023: 3:31:14.57
    No Description Provided

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -1,4 +1,5 @@
 @echo off
+@REM VARS / ENV
 for /F "tokens=2" %%i in ('date /t') DO SET date=%%i
 IF "%~1" == "" (SET commitName="%date%:%time%")
 IF NOT "%~1" == "" (SET commitName="%~1")
@@ -7,14 +8,17 @@ IF NOT "%~2" == "" (SET description="%~2")
 IF "%~3" == "" (SET branch="main")
 IF NOT "%~3" == "" (SET branch="%~3")
 
+@REM GIT
 git add .
 git commit -m %commitName% -m %description%
 git branch -m %branch%
 git push -u origin %branch%
 
+@REM Get GIT details
 for /f %%i in ('git rev-parse origin/%branch%') DO (SET "hash=%%i")
 for /f %%i in ('git config --get remote.origin.url') DO (SET "URL=%%i")
 
+@REM Display info about push / git
 ECHO ---------------------------------------------------------------------------------
 ECHO BRANCH: %branch%
 ECHO COMMIT NAME: %commitName%
