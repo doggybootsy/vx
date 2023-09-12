bbb07329637067ac878b7a85ed495d8b54cbb6a9
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-09-12 00:43:27 -0700

    menu typings

--- renderer/src/dashboard/ui/menu.tsx
+++ renderer/src/dashboard/ui/menu.tsx
@@ -1,31 +1,111 @@
 import { components } from "renderer/menus";
 import webpack from "renderer/webpack";
-import pluginManager from "renderer/addons/plugins";
-import themeManager from "renderer/addons/themes";
+import pluginManager, { Plugin } from "renderer/addons/plugins";
+import themeManager, { Theme } from "renderer/addons/themes";
 import { useStateFromStores } from "renderer/hooks";
 import CustomCSS from "renderer/ui/customCSS";
 import { openWindow } from "renderer/window";
 import native from "renderer/native";
 import storage from "renderer/storage";
+import { Icons } from "renderer/components";
+
+function useAddonMenu(type: "themes" | "plugins") {
+  const React = webpack.common.React!;
+
+  const manager = React.useMemo(() => type === "plugins" ? pluginManager : themeManager, [ type ]);
+  
+  const addons = useStateFromStores([ manager ], () => manager.getAll());
+  const details = useStateFromStores(addons, () => addons.map((addon: Theme | Plugin) => ({
+    id: addon.id,
+    enabled: addon.enabled,
+    author: addon.meta.author,
+    toggle: () => addon.toggle(),
+    name: addon.meta.name || addon.id
+  })));
+
+  const [ query, setQuery ] = React.useState("");
+  const filteredDetails = React.useMemo(() => {
+    function includes(item?: string) {
+      if (!item) return false;
+      if (item.toLocaleLowerCase().includes(query.toLocaleLowerCase())) return true;
+      return false;
+    };
+
+    return details.filter((detail) => {
+      if (includes(detail.author)) return true;
+      if (includes(detail.id)) return true;
+      if (includes(detail.name)) return true;
+      return false;
+    });
+  }, [ details, query ]);
+  const addonSortingMethod = storage.use<"normal" | "reverse">("addon-sort-method", "normal");
+
+  const sorted = React.useMemo(() => {
+    const multiplier = addonSortingMethod === "reverse" ? -1 : 1;
+
+    return filteredDetails.sort((a, b) => a.id.localeCompare(b.id) * multiplier);
+  }, [ addonSortingMethod, filteredDetails ]);
+
+  const sortIcon = React.useMemo(() => {
+    if (addonSortingMethod === "reverse") return Icons.SortReverse;
+    return Icons.Sort;
+  }, [ addonSortingMethod ]);
+  
+  return (
+    <components.MenuItem 
+      id={`vx/${type}`}
+      label={type === "plugins" ? "Plugins" : "Themes"}
+      action={() => {
+        webpack.common.navigation!.transitionTo(`/vx/${type}`);
+      }}
+    >
+      <components.MenuControlItem 
+        id={`vx/${type}/search`}
+        control={(props, ref) => (
+          <components.MenuSearchControl
+            query={query}
+            onChange={setQuery}
+            ref={ref}
+            {...props}
+          />
+        )}
+      />
+      {sorted.map((addon) => (
+        <components.MenuCheckboxItem 
+          id={`vx/${type}/${addon.id}`}
+          key={`vx/${type}/${addon.id}`}
+          label={addon.name}
+          subtext={addon.author}
+          action={addon.toggle}
+          checked={addon.enabled}
+        />
+      ))}
+      <components.MenuSeparator />
+      <components.MenuItem
+        label={addonSortingMethod === "reverse" ? "Sorting Z-A" : "Sorting A-Z"}
+        id={`vx/${type}/sort`}
+        action={() => {
+          storage.set("addon-sort-method", addonSortingMethod === "reverse" ? "normal" : "reverse");
+        }}
+        icon={sortIcon}
+        dontCloseOnActionIfHoldingShiftKey={true}
+      />
+      <components.MenuItem
+        id={`vx/${type}/open`}
+        label="Open Folder"
+        action={() => {
+          native.openPath(native.path.join(native.dirname, "..", type));
+        }}
+      />
+    </components.MenuItem>
+  )
+};
 
 function useMenu() {
   const React = webpack.common.React!;
   
-  const plugins = useStateFromStores([ pluginManager ], () => pluginManager.getAll());
-  const pluginDetails = useStateFromStores(plugins, () => plugins.map((plugin) => ({
-    id: plugin.id,
-    enabled: plugin.enabled,
-    toggle: () => plugin.toggle(),
-    name: plugin.meta.name || plugin.id
-  })));
-
-  const themes = useStateFromStores([ themeManager ], () => themeManager.getAll());
-  const themeDetails = useStateFromStores(themes, () => themes.map((theme) => ({
-    id: theme.id,
-    enabled: theme.enabled,
-    toggle: () => theme.toggle(),
-    name: theme.meta.name || theme.id
-  })));
+  const pluginsMenu = useAddonMenu("plugins");
+  const themesMenu = useAddonMenu("themes");
 
   return (
     <>
@@ -36,56 +116,8 @@ function useMenu() {
           webpack.common.navigation!.transitionTo("/vx");
         }}
       />
-      <components.MenuItem 
-        id="vx/plugins"
-        label="Plugins"
-        action={() => {
-          webpack.common.navigation!.transitionTo("/vx/plugins");
-        }}
-      >
-        {pluginDetails.map((plugin) => (
-          <components.MenuCheckboxItem 
-            id={`vx/plugins/${plugin.id}`}
-            key={`vx/plugins/${plugin.id}`}
-            label={plugin.name}
-            action={plugin.toggle}
-            checked={plugin.enabled}
-          />
-        ))}
-        {pluginDetails.length && <components.MenuSeparator />}
-        <components.MenuItem
-          id="vx/plugins/open"
-          label="Open Folder"
-          action={() => {
-            native.openPath(native.path.join(native.dirname, "..", "plugins"))
-          }}
-        />
-      </components.MenuItem>
-      <components.MenuItem 
-        id="vx/themes"
-        label="Themes"
-        action={() => {
-          webpack.common.navigation!.transitionTo("/vx/themes");
-        }}
-      >
-        {themeDetails.map((theme) => (
-          <components.MenuCheckboxItem 
-            id={`vx/themes/${theme.id}`}
-            key={`vx/themes/${theme.id}`}
-            label={theme.name}
-            action={theme.toggle}
-            checked={theme.enabled}
-          />
-        ))}
-        {themeDetails.length && <components.MenuSeparator />}
-        <components.MenuItem
-          id="vx/themes/open"
-          label="Open Folder"
-          action={() => {
-            native.openPath(native.path.join(native.dirname, "..", "themes"))
-          }}
-        />
-      </components.MenuItem>
+      {pluginsMenu}
+      {themesMenu}
       <components.MenuItem 
         id="vx/store"
         label="Store"
--- renderer/src/menus/components.ts
+++ renderer/src/menus/components.ts
@@ -1,53 +1,139 @@
+import { IconProps } from "renderer/components/icons";
 import VXError from "renderer/error";
 import webpack from "renderer/webpack";
+import { MenuRenderProps } from "renderer/menus";
+
+interface MenuProps extends MenuRenderProps {
+  navId: string,
+  onClose(): void,
+  children: React.ReactNode
+}
+
+type MenuItemColor = "default" | "danger" | "premium-gradient";
+
+interface MenuItemRenderProps {
+  color: MenuItemColor,
+  disabled: boolean,
+  isFocused: boolean
+};
+
+type MenuItemLabelAndRenderProps = { label: string } | { render(props: MenuItemRenderProps): React.ReactNode };
+interface BaseMenuItemProps extends VX.Dict {
+  id: string,
+  disabled?: boolean,
+  action?(event: React.MouseEvent): void,
+  children?: React.ReactNode,
+  icon?: React.FunctionComponent<IconProps>,
+  color?: MenuItemColor,
+  subtext?: string,
+  focusedClassName?: string,
+  className?: string,
+  keepItemStyles?: boolean,
+  dontCloseOnActionIfHoldingShiftKey?: boolean,
+  imageUrl?(props: unknown): string
+};
+
+type MenuItemProps = BaseMenuItemProps & MenuItemLabelAndRenderProps
+
+interface MenuCheckboxItemProps {
+  id: string,
+  label: string,
+  disabled?: boolean,
+  subtext?: React.ReactNode,
+  action?(event: React.MouseEvent): void,
+  checked: boolean
+};
+
+interface MenuControlProps {
+  disabled?: boolean,
+  isFocused: boolean,
+  onClose(): void
+};
+interface MenuControlRef {
+  activate(): void,
+  blur(): void,
+  focus(): void
+};
+
+interface MenuControlItemProps {
+  id: string,
+  label?: string,
+  disabled?: boolean,
+  control(props: MenuControlProps, ref: VX.Ref<MenuControlRef | null>): React.ReactElement
+};
+
+interface MenuSliderControlProps extends MenuControlProps {
+  value: number,
+  maxValue?: number,
+  minValue?: number
+  onChange(value: number): void,
+  renderValue?(value: number): React.ReactNode,
+  ref: VX.Ref<MenuControlRef | null>
+};
+interface MenuSearchControlProps extends MenuControlProps {
+  query: string,
+  onChange(value: string): void,
+  loading?: boolean,
+  placeholder?: string
+  ref: VX.Ref<MenuControlRef | null>
+}
+
+interface MenuGroupProps {
+  label?: string,
+  children?: React.ReactNode
+};
+interface MenuRadioItemProps extends MenuCheckboxItemProps {
+  group: string
+};
+
 
 export default {
-  get Menu() { 
+  get Menu(): React.FunctionComponent<MenuProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.Menu;
   },
-  get MenuCheckboxItem() { 
+  get MenuCheckboxItem(): React.FunctionComponent<MenuCheckboxItemProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuCheckboxItem;
   },
-  get MenuControlItem() { 
+  get MenuControlItem(): React.FunctionComponent<MenuControlItemProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuControlItem;
   },
-  get MenuGroup() { 
+  get MenuGroup(): React.FunctionComponent<MenuGroupProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuGroup;
   },
-  get MenuItem() { 
+  get MenuItem(): React.FunctionComponent<MenuItemProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuItem;
   },
-  get MenuRadioItem() { 
+  get MenuRadioItem(): React.FunctionComponent<MenuRadioItemProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuRadioItem;
   },
-  get MenuSearchControl() { 
+  get MenuSearchControl(): React.FunctionComponent<MenuSearchControlProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuSearchControl;
   },
-  get MenuSeparator() { 
+  get MenuSeparator(): React.FunctionComponent { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuSeparator;
   },
-  get MenuSliderControl() { 
+  get MenuSliderControl(): React.FunctionComponent<MenuSliderControlProps> { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuSliderControl;
   },
-  get MenuSpinner() { 
+  get MenuSpinner(): React.FunctionComponent { 
     const components = webpack.common.components;
     if (!components) throw new VXError(VXError.codes.NO_COMPONENTS);
     return components.MenuSpinner;
