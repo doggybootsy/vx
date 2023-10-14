b84dea3afe10c362cffafd65d5630f7b519fa097
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-10-13 17:58:50 -0700

    Plugin~LocalNickNames
    Local nicknames

--- packages/mod/src/plugins/localnicknames/index.ts
+++ packages/mod/src/plugins/localnicknames/index.ts
@@ -1,8 +1,9 @@
 import { definePlugin } from "../";
-import { User } from "discord-types/general";
+import { User, GuildMember } from "discord-types/general";
 import { DataStore } from "../../api/storage";
 import { Developers } from "../../constants";
-import { getLazyByProtoKeys } from "../../webpack";
+import { getLazyByKeys, getLazyByProtoKeys } from "../../webpack";
+import { GuildMemberStore } from "discord-types/stores";
 
 const dataStore = new DataStore<{
   nicknames: Record<string, string>
@@ -12,26 +13,45 @@ dataStore.ensure("nicknames", {});
 
 const localNickNames = dataStore.get("nicknames")!;
 
+async function patchUser() {
+  const User = await getLazyByProtoKeys<any>([ "isClaimed", "isSystemUser" ]);
+  
+  Object.defineProperty(User.prototype, "globalName", {
+    get(this: User & { _globalName: string }) {
+      if (this.id in localNickNames) {
+        return localNickNames[this.id];
+      };
+
+      return this._globalName;
+    },
+    set(this: User & { _globalName: string }, v) {
+      this._globalName = v;
+      return true;
+    }
+  });
+};
+async function patchGuildMember() {
+  const GuildMemberStore = await getLazyByKeys<GuildMemberStore>([ "getMember", "getMemberIds" ]);
+  
+  const getMember = GuildMemberStore.getMember;
+  GuildMemberStore.getMember = function(guildId, userId) {
+    const res = getMember.call(this, guildId, userId);
+
+    if (res && userId in localNickNames) {
+      res.nick = localNickNames[userId];
+    };
+
+    return res;
+  };
+};
+
 export default definePlugin({
   name: "LocalNickNames",
   description: "Adds custom local nicknames ",
   authors: [ Developers.doggybootsy ],
-  async start() {
-    const User = await getLazyByProtoKeys<any>([ "isClaimed", "isSystemUser" ]);
-    
-    Object.defineProperty(User.prototype, "globalName", {
-      get(this: User & { _globalName: string }) {
-        if (this.id in localNickNames) {
-          return localNickNames[this.id];
-        };
-
-        return this._globalName;
-      },
-      set(this: User & { _globalName: string }, v) {
-        this._globalName = v;
-        return true;
-      }
-    });
+  start() {
+    patchUser();
+    patchGuildMember();
   },
   addNickName(id: string, nickname: string) {
     localNickNames[id] = nickname;
