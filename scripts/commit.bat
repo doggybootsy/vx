05fc5e77045e225aca6d3164fbd8ecd5d0fe9dd1
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-10-16 02:49:41 -0700

    Scripts
    .git is sometimes there and sometimes isn't?

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -18,9 +18,13 @@ git push -u origin %branch%
 for /f %%i in ('git rev-parse origin/%branch%') DO (SET "hash=%%i")
 for /f %%i in ('git config --get remote.origin.url') DO (SET "URL=%%i")
 
+@REM URL sometimes has '.git' at the end, so we remove that
+IF "%URL:~-4%"==".git" (SET CORRECT_URL=%URL:~0,-4%)
+IF NOT "%URL:~-4%"==".git" (SET CORRECT_URL=%URL%)
+
 @REM Display info about push / git
 ECHO ---------------------------------------------------------------------------------
 ECHO BRANCH: %branch%
 ECHO COMMIT NAME: %commitName%
 ECHO COMMIT DESCRIPTION: %description%
-ECHO %URL:~0,-4%/commit/%hash%
+ECHO %CORRECT_URL:~0,-4%/commit/%hash%
