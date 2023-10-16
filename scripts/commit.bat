a7f67bb66a2b5dac069a48924c6101bbd5fe1d40
Robert Delaney <71196819+doggybootsy@users.noreply.github.com>
2023-10-16 03:05:19 -0700

    Scripts
    Forgot a small thing

--- scripts/commit.bat
+++ scripts/commit.bat
@@ -1,4 +1,6 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
 @REM VARS / ENV
 for /F "tokens=2" %%i in ('date /t') DO SET date=%%i
 IF "%~1" == "" (SET commitName="%date%:%time%")
@@ -22,9 +24,23 @@ for /f %%i in ('git config --get remote.origin.url') DO (SET "URL=%%i")
 IF "%URL:~-4%"==".git" (SET CORRECT_URL=%URL:~0,-4%)
 IF NOT "%URL:~-4%"==".git" (SET CORRECT_URL=%URL%)
 
+@REM dash
+SET COMMIT_URL=%CORRECT_URL%/commit/%hash%
+SET "dash_string="
+SET /a "url_length=0"
+
+:loop
+IF "!COMMIT_URL:~%url_length%,1!" neq "" (
+  SET /a "url_length+=1"
+  goto loop
+)
+FOR /l %%i IN (1,1,%url_length%) DO (
+  SET "dash_string=!dash_string!-"
+)
+
 @REM Display info about push / git
-ECHO ---------------------------------------------------------------------------------
+ECHO %dash_string%
 ECHO BRANCH: %branch%
 ECHO COMMIT NAME: %commitName%
 ECHO COMMIT DESCRIPTION: %description%
-ECHO %CORRECT_URL%/commit/%hash%
+ECHO %COMMIT_URL%
