1f24cad8f777961b161526bf95e1961d318feebc
Robert Delaney <Doggybootsy@gmail.com>
2024-08-03 15:30:04 -0700

    Reveal All Spoilers
    When holding ctrl it will reveal all spoilers in a message

---
+++ packages/mod/src/plugins/reveal-all-spoilers/index.ts
@@ -0,0 +1,22 @@
+import { definePlugin } from "..";
+import { Developers } from "../../constants";
+
+export default definePlugin({
+  authors: [ Developers.doggybootsy ],
+  requiresRestart: false,
+  patches: {
+    match: "this.removeObscurity",
+    find: /this\.removeObscurity/g,
+    replace: "$self.removeObscurity.bind(null,()=>$enabled,$&),__vx_ras__:this"
+  },
+  removeObscurity(isEnabled: () => boolean, original: (event: React.MouseEvent) => void, event: React.MouseEvent) {
+    if (event.ctrlKey && isEnabled()) {
+      for (const child of event.currentTarget.parentElement!.childNodes) {
+        if (!(child instanceof HTMLElement)) continue;
+        if (!child.matches("[class^=spoilerContent_]")) continue;
+        child.__reactFiber$?.alternate?.memoizedProps?.__vx_ras__?.removeObscurity(event)
+      }
+    }
+    else original(event);
+  }
+});
