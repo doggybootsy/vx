4b53f90b732042730b8922b4404dcc8bdcc68cda
Robert Delaney <Doggybootsy@gmail.com>
2024-08-04 19:20:24 -0700

    More notes
    Based off of https://github.com/zrodevkaan/RepluggedPlugins/blob/main/plugins/dev.kaan.MoreNotes/src/index.tsx with full permission of course

---
+++ packages/mod/src/plugins/more-notes/index.tsx
@@ -0,0 +1,37 @@
+import { definePlugin } from "..";
+import { DataStore } from "../../api/storage";
+import { Developers } from "../../constants";
+
+const notes = new DataStore<Record<string, string>>("more-notes", { version: 1 });
+
+export default definePlugin({
+  authors: [ Developers.doggybootsy ],
+  requiresRestart: false,
+  patches: {
+    match: "findDOMNode(this.noteRef.current);",
+    replacements: [
+      {
+        find: /loading:(.{1,3}),note:(.{1,3}),hideNote:.{1,3}}=this\.props;/,
+        replace: "$&if($enabled){$1=false;$2=$self.getNote(this.props.userId,$2)};"
+      },
+      {
+        find: /,(.{1,3}\..{1,3}\.updateNote)/,
+        replace: ",($enabled?$self.setNote:$1)"
+      },
+      {
+        find: ".length>=5",
+        replace: ".length>=($enabled?Infinity:5)"
+      },
+      {
+        find: "maxLength:",
+        replace: "maxLength:$enabled?Infinity:"
+      }
+    ]
+  },
+  setNote(userId: string, note: string) {
+    notes.set(userId, note);
+  },
+  getNote(userId: string, original: string | null) {
+    return notes.get(userId) || original;
+  }
+});
