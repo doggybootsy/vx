253ba438c9b18d611744c73fe4023be493b6685f
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-09-11 19:01:56 -0500

    picky

--- packages/mod/src/plugins/first-message/index.tsx
+++ packages/mod/src/plugins/first-message/index.tsx
@@ -1,7 +1,7 @@
 import {definePlugin} from "../index";
 import {Developers} from "../../constants";
 import {NavigationUtils} from "@webpack/common";
-import {SelectedChannelStore, SelectedGuildStore} from "@webpack/common"
+import {SelectedChannelStore} from "@webpack/common"
 import {Channel, Guild} from "discord-types/general";
 
 export default definePlugin({
@@ -11,10 +11,7 @@ export default definePlugin({
         id: "first-message",
         name: "first-message",
         execute(options: any[], {channel, guild}: { channel: Channel; guild?: Guild }) {
-            NavigationUtils.transitionToGuild(guild?.id ?? null, channel?.id ?? SelectedChannelStore.getChannelId(), "0")
+            NavigationUtils.transitionToGuild(guild?.id ?? null, channel.id, "0")
         }
     },
-    start() {
-        
-    }
 })
