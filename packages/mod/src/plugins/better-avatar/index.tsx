26d9e54ffe17ce59159b6ecad74f413449b30232
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-09-14 09:35:20 -0500

    Better Cropping

---
+++ packages/mod/src/plugins/better-avatar/index.tsx
@@ -0,0 +1,25 @@
+import { definePlugin } from "../index";
+import { Developers } from "../../constants";
+import {bySource, getLazy, getModule, getProxy} from "@webpack";
+import { Injector } from "../../patcher";
+import { createAbort, findInReactTree } from "../../util";
+
+const INJECT: Injector = new Injector();
+
+export default definePlugin({
+    authors: [Developers.kaan],
+    requiresRestart: false,
+    async start() {
+        const GIF_CROP: any = await getLazy(bySource('ImageCroppingModal'));
+        
+        INJECT.after(GIF_CROP, 'default', (_: any, __: any, returnValue: any) => {
+            // @ts-ignore
+            const SliderContainer: { children: Array<{ props: { maxValue: number, stickToMarkers: boolean } }> } = findInReactTree(returnValue, (x: { className: string | string[]; }) => x?.className?.includes?.('sliderContainer'));
+            const Slider = SliderContainer.children[1].props
+            Slider.maxValue = 20;
+        });
+    },
+    stop() {
+        INJECT.unpatchAll()
+    }
+});
