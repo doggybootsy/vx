a1eec84bf6e2b6056485f3223d9949ad3f4f9af9
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-09-22 16:08:27 -0500

    fixed

--- packages/mod/src/plugins/ghost-call/index.tsx
+++ packages/mod/src/plugins/ghost-call/index.tsx
@@ -1,9 +1,11 @@
 import { definePlugin } from "../index";
 import { Developers } from "../../constants";
 import { Injector } from "../../patcher";
-import { bySource, getLazy, getModule } from "@webpack";
+import { bySource, getModule } from "@webpack";
+import {createElement} from "react";
 
 const inj = new Injector();
+
 export default definePlugin({
     authors: [Developers.kaan],
     requiresRestart: false,
@@ -11,21 +13,33 @@ export default definePlugin({
         {
             match: ".useIsRingtoneEligible",
             find: /((\w+)=\(0,\w+\.\w+\)\(\(\)=>{let \w+="call_ringing";.{500,800})(animatedStyle)/,
-            replace: (match, prefix, variable, suffix) => `${prefix}audioContext:${variable},${suffix}`,
+            replace: (match, prefix, variable, suffix) =>
+                `${prefix}audioContext:${variable},${suffix}`,
         },
         {
-            identifier: "ghost", 
+            identifier: "ghost",
             match: ".RingingType.INCOMING",
             find: /INCOMING_CALL_PREVIEW_CAMERA}\):null/,
-            replace: (prefix: string) =>
-                `${prefix},VX.React.createElement('div',{},VX.React.createElement(VX.webpack.getModule(VX.webpack.filters.bySource(".lineHeightReset")).Z,{label: "Ghost End", centerButton: true, iconComponent: VX.webpack.getModule(VX.webpack.filters.bySource("M22.89 11.7c.07.2.07.4")).t, background: "gray", onClick: () => {arguments?.[0]?.audioContext.stop()}}))`,
-        }
+            replace: `$&, $self._renderButton(arguments?.[0])`,
+        },
     ],
-    _renderButton: ({audioContext}) => {
-        return (
-            <div style={{background: 'red'}}>
-                hello WORLDP:FKJDLFJKHNDFLKLDFKL
-            </div>
-        )
-    }
+    
+    _renderButton: ({ audioContext }) => {
+        const ButtonModule: any = getModule(bySource(".lineHeightReset"));
+        const IconComponent: any = getModule(bySource("M22.89 11.7c.07.2.07.4"));
+
+        return createElement(
+            "div",
+            {},
+            createElement(ButtonModule.Z, {
+                label: "Ghost End",
+                centerButton: true,
+                iconComponent: IconComponent.t,
+                background: "gray",
+                onClick: () => {
+                    audioContext?.stop();
+                },
+            })
+        );
+    },
 });
