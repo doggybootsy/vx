c2b3264ec16a6ae996ebc815ed0916fbe8d147d4
hsiF <90235641+zrodevkaan@users.noreply.github.com>
2024-10-01 17:39:57 -0500

    sdfsdf

--- packages/mod/src/plugins/toolbar/index.tsx
+++ packages/mod/src/plugins/toolbar/index.tsx
@@ -1,14 +1,22 @@
 import { definePlugin } from "../index";
 import { Developers } from "../../constants";
-import {bySource, byStrings, getLazy, getProxy, getProxyByStrings} from "@webpack";
+import {bySource, byStrings, getLazy, getMangledLazy, getModuleIdBySource, getProxy, webpackRequire} from "@webpack";
 import { createElement } from "react";
 import {Injector} from "../../patcher";
 import {Button, SystemDesign, Tooltip} from "../../components";
 import {openNotification} from "../../api/notifications";
+import {proxyCache} from "../../util";
 
 const PanelMenu = getLazy(bySource('--custom-app-panels-height'));
 const PanelButton = getProxy(byStrings("{tooltipText:", ".Masks.PANEL_BUTTON,"));
-const ProtoSync = getProxy(x=>x.G6.updateSetting);
+const ProtoSync = proxyCache(() => {
+    const mId = getModuleIdBySource('"status","showCurrentGame"');
+    const module = webpackRequire!.m[mId];
+
+    const match = module.toString().match(/,(.{1,3}):function\(\){return (.{1,3})},.+,\2=\(0,.{1,3}\..{1,3}\)\("status","showCurrentGame"/);
+
+    return webpackRequire!(mId)[match[1]];
+});
 
 class Toolbar {
     private static items: { id: string; element: any }[] = [];
@@ -43,23 +51,19 @@ export default definePlugin({
     authors: [Developers.kaan],
     requiresRestart: false,
     async start(signal: AbortSignal) {
+        const Module = await PanelMenu;
+
         if (signal.aborted) return;
         
-        const GameToggleButton =
-            <Tooltip text={"Game Toggle"}>
-                {(props) => (
-                    <PanelButton {...props} onClick={() => {
-                        const ActivitySetting = !ProtoSync.G6.getSetting()
-                        ProtoSync.G6.updateSetting(ActivitySetting)
-                        openNotification({title: "ProtoSync Update", description: `Activity is currently ${ActivitySetting ? "enabled" : "disabled"}.`, sliderColor: getColorBasedOffStatus(ActivitySetting)})
-                    }} icon={() => (<SystemDesign.GameControllerIcon/>)}/>
-                )}
-            </Tooltip>
+        const GameToggleButton = <PanelButton onClick={() => {
+                const ActivitySetting = !ProtoSync.getSetting()
+                ProtoSync.updateSetting(ActivitySetting)
+                openNotification({title: "ProtoSync Update", description: `Activity is currently ${ActivitySetting ? "enabled" : "disabled"}.`, sliderColor: getColorBasedOffStatus(ActivitySetting)})
+            }} icon={() => (<SystemDesign.GameControllerIcon/>)}/>
         
         Toolbar.addItem("vx-game-button-toggle", GameToggleButton)
         
-        // @ts-ignore
-        inj.after(await PanelMenu, 'b', (a: any, b: any, c: { props: { children: React.DetailedReactHTMLElement<{ style: { display: "flex"; justifyContent: "center"; alignItems: "center"; color: "white"; }; }, HTMLElement>[]; }; }) => {
+        inj.after(Module, 'b', (a: any, b: any, c: { props: { children: React.DetailedReactHTMLElement<{ style: { display: "flex"; justifyContent: "center"; alignItems: "center"; color: "white"; }; }, HTMLElement>[]; }; }) => {
             console.log(a, b, c);
 
             const centeredDiv = createElement("div", {
